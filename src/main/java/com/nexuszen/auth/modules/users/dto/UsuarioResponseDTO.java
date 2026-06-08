2a644449ee1d687755e658043a71745e63eba686
Cristian Castro <86340@sistemas.frc.utn.edu.ar>
2026-06-08 13:38:24 -0300

    fix(auth): add AllArgsConstructor and NoArgsConstructor to UsuarioResponseDTO to fix compiler issues in tests

--- src/main/java/com/nexuszen/auth/modules/users/dto/UsuarioResponseDTO.java
+++ src/main/java/com/nexuszen/auth/modules/users/dto/UsuarioResponseDTO.java
@@ -4,11 +4,15 @@ import com.nexuszen.auth.models.dto.UsuarioEmailDTO;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioResponseDTO {
   private UUID id;
   private String usuario;
